c50b34df5de7d06704e6ca4d48f4dcd4f3e6a523
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-08 20:36:17 +0200

    added skeletons for RSA and ED25519 cryptos

---
+++ XSLibrary/XSCryptography/ConnectionCryptos/RSACrypto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace XSLibrary.Cryptography.ConnectionCryptos
+{
+    class RSACrypto : IConnectionCrypto
+    {
+        RSA KEXCrypto;
+        AesCryptoServiceProvider DataCrypto;
+
+        public RSACrypto(bool active) : base(active)
+        {
+            KEXCrypto = new RSACryptoServiceProvider(2048);
+            //KEXCrypto.
+        }
+
+        protected override bool HandshakeActive(Action<byte[]> Send, ReceiveCall Receive)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool HandshakePassive(Action<byte[]> Send, ReceiveCall Receive)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] EncryptData(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override byte[] DecryptData(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
