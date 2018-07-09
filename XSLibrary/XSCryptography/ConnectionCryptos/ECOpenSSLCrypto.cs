b583da89e3e190a412eb925219357393b8089de4
David Hofer <dave.hofer@hotmail.com>
2018-07-09 16:19:54 +0200

    now using openSSL library for cryptos

---
+++ XSLibrary/XSCryptography/ConnectionCryptos/ECOpenSSLCrypto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using XSLibrary.Cryptography.ConnectionCryptos.Wrappers;
+
+namespace XSLibrary.Cryptography.ConnectionCryptos
+{
+    class ECOpenSSLCrypto : IConnectionCrypto
+    {
+        ECDHOpenSSL KEXCrypto;
+        AESOpenSSL DataCrypto;
+
+        string SECRET = "very secret, much wow!";
+
+        public ECOpenSSLCrypto(bool active) : base(active)
+        {
+            KEXCrypto = new ECDHOpenSSL();
+            DataCrypto = new AESOpenSSL();
+        }
+
+        public override byte[] EncryptData(byte[] data)
+        {
+            return DataCrypto.Encrypt(data);
+        }
+
+        public override byte[] DecryptData(byte[] data)
+        {
+            return DataCrypto.Decrypt(data);
+        }
+
+        protected override bool HandshakeActive(Action<byte[]> Send, ReceiveCall Receive)
+        {
+            Send(KEXCrypto.GetPublicKey());
+            if (!Receive(out byte[] data, out IPEndPoint source))
+                return false;
+
+            DataCrypto.SetSharedSecret(KEXCrypto.GenerateSharedSecret(data));
+            Send(EncryptData(Encoding.ASCII.GetBytes(SECRET)));
+            return true;
+        }
+
+        protected override bool HandshakePassive(Action<byte[]> Send, ReceiveCall Receive)
+        {
+            if (!Receive(out byte[] data, out IPEndPoint source))
+                return false;
+
+            DataCrypto.SetSharedSecret(KEXCrypto.GenerateSharedSecret(data));
+            Send(KEXCrypto.GetPublicKey());
+
+            if (!Receive(out data, out source))
+                return false;
+
+            string secret = Encoding.ASCII.GetString(DecryptData(data));
+
+            return secret == SECRET;
+        }
+    }
+}
---
+++ XSLibrary/XSCryptography/ConnectionCryptos/Wrappers/ECDHOpenSSL.cs
@@ -0,0 +1,48 @@
+﻿using OpenSSL.Core;
+using OpenSSL.Crypto;
+using OpenSSL.Crypto.EC;
+using System;
+
+namespace XSLibrary.Cryptography.ConnectionCryptos.Wrappers
+{
+    class ECDHOpenSSL : IDisposable 
+    {
+        Key _ecdh;
+        Group _group { get { return _ecdh.Group; } }
+        BigNumber.Context _context;
+
+        public ECDHOpenSSL()
+        {
+            _ecdh = Key.FromCurveName(Objects.NID.secp521r1);
+            _ecdh.GenerateKey();
+
+            _context = new BigNumber.Context();
+        }
+
+        public byte[] GenerateSharedSecret(byte[] otherPublicKey)
+        {
+            byte[] sharedSecret = new byte[32];
+            _ecdh.ComputeKey(new Point(_group, otherPublicKey, _context), sharedSecret, SHA256);
+            return sharedSecret;
+        }
+
+        public byte[] GetPublicKey()
+        {
+            return _ecdh.PublicKey.GetBytes(_context, Point.Form.POINT_CONVERSION_UNCOMPRESSED);
+        }
+
+        private byte[] SHA256(byte[] msg)
+        {
+            using (MessageDigestContext mdc = new MessageDigestContext(MessageDigest.SHA256))
+            {
+                return mdc.Digest(msg);
+            }
+        }
+
+        public void Dispose()
+        {
+            _ecdh.Dispose();
+            _context.Dispose();
+        }
+    }
+}
