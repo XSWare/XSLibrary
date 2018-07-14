7941c7d1acd5fa44f31989f66e53b6b3effb7f79
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-14 20:04:33 +0200

    added basic structure

---
+++ XSLibrary/XSCryptography/AccountManagement/UserData.cs
@@ -0,0 +1,16 @@
+﻿namespace XSLibrary.Cryptography.AccountManagement
+{
+    class UserData
+    {
+        public string Username { get; private set; }
+        public byte[] PasswordHash { get; private set; }
+        public byte[] Salt { get; private set; }
+
+        public UserData(string userName, byte[] passwordHash, byte[] salt)
+        {
+            Username = userName;
+            PasswordHash = passwordHash;
+            Salt = salt;
+        }
+    }
+}
