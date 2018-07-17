1eb778ace55ea0e1cdde16c1285332ca29332782
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-18 01:24:13 +0200

    simplified and removed unsubscribe lock to avoid deadlocks
    - currently unsubscribe can not be used in a good way, have to look into this

--- XSLibrary/XSThreadSafety/OneShotEvent.cs
+++ XSLibrary/XSThreadSafety/OneShotEvent.cs
@@ -2,15 +2,12 @@
 
 namespace XSLibrary.ThreadSafety
 {
-    public class OneShotEvent
+    /// <summary>
+    /// Triggers once, if the event is invoked or was invoked before subscribing to it.
+    /// Do not unsubscribe from this event in the event handler, it happens automatically.
+    /// </summary>
+    public class OneTimeEvent
     {
-        enum EventStates
-        {
-            Untouched,
-            Invoked,
-            Fired
-        }
-
         public delegate void FireHandle();
 
         public event FireHandle OnFire
@@ -19,40 +16,28 @@ namespace XSLibrary.ThreadSafety
             {
                 m_lock.Execute(() =>
                 {
-                    InternalEvent += value;
-                    if(state == EventStates.Invoked)
-                        FireEvent();
+                    if (m_invoked)
+                        value();
+                    else
+                        InternalEvent += value;
                 });
             }
-            remove { m_lock.Execute(() => InternalEvent -= value); }
+            remove { InternalEvent -= value; }
         }
 
         private SafeExecutor m_lock = new SingleThreadExecutor();
-        private volatile EventStates state = EventStates.Untouched;
+        private volatile bool m_invoked = false;
 
         private event FireHandle InternalEvent;
-
-        public OneShotEvent()
-        {
-        }
 
         public void Invoke()
         {
             m_lock.Execute(() =>
             {
-                if(state == EventStates.Untouched)
-                    state = EventStates.Invoked;
-                FireEvent();
+                m_invoked = true;
+                InternalEvent?.Invoke();
+                InternalEvent = null;
             });
-        }
-
-        private void FireEvent()
-        {
-            if (state != EventStates.Fired && InternalEvent != null)
-            {
-                state = EventStates.Fired;
-                InternalEvent();
-            }
         }
     }
 }
