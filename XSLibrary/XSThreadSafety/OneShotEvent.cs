84b2bf9359f9f72567fd30876cd8501a3da6d8ef
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-18 00:21:20 +0200

    added OneShotEvent

---
+++ XSLibrary/XSThreadSafety/OneShotEvent.cs
@@ -0,0 +1,58 @@
+﻿using XSLibrary.ThreadSafety.Executors;
+
+namespace XSLibrary.ThreadSafety
+{
+    public class OneShotEvent
+    {
+        enum EventStates
+        {
+            Untouched,
+            Invoked,
+            Fired
+        }
+
+        public delegate void FireHandle();
+
+        public event FireHandle OnFire
+        {
+            add
+            {
+                m_lock.Execute(() =>
+                {
+                    InternalEvent += value;
+                    if(state == EventStates.Invoked)
+                        FireEvent();
+                });
+            }
+            remove { m_lock.Execute(() => InternalEvent -= value); }
+        }
+
+        private SafeExecutor m_lock = new SingleThreadExecutor();
+        private volatile EventStates state = EventStates.Untouched;
+
+        private event FireHandle InternalEvent;
+
+        public OneShotEvent()
+        {
+        }
+
+        public void Invoke()
+        {
+            m_lock.Execute(() =>
+            {
+                if(state == EventStates.Untouched)
+                    state = EventStates.Invoked;
+                FireEvent();
+            });
+        }
+
+        private void FireEvent()
+        {
+            if (state != EventStates.Fired && InternalEvent != null)
+            {
+                state = EventStates.Fired;
+                InternalEvent();
+            }
+        }
+    }
+}
