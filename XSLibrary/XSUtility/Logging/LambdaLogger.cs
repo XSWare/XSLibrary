e01afdf0a8e10ae5823bb085ea64d82e3fbbe358
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-21 10:29:04 +0200

    added LambdaLogger

---
+++ XSLibrary/XSUtility/Logging/LambdaLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XSLibrary.Utility.Logging
+{
+    public class LambdaLogger : Logger
+    {
+        Action<string> m_log;
+
+        public LambdaLogger(Action<string> log)
+        {
+            m_log = log;
+        }
+
+        protected override void LogMessage(string text)
+        {
+            m_log(text);
+        }
+    }
+}
