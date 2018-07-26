98165b7b809ec2411b2059b11f75c97f6d7aa830
David Hofer <dave.hofer@hotmail.com>
2018-07-26 16:27:48 +0200

    simplified

--- XSLibrary/XSThreadSafety/Locks/UnleashSignal.cs
+++ XSLibrary/XSThreadSafety/Locks/UnleashSignal.cs
@@ -4,22 +4,13 @@ namespace XSLibrary.ThreadSafety.Locks
 {
     public class UnleashSignal : ILock
     {
-        private volatile int currentWait = 0;
         private object m_lock = new object();
 
         public void Lock()
         {
             lock (m_lock)
             {
-                int waitOn = currentWait + 1;
-
-                while (true)
-                {
-                    if (waitOn == currentWait)
-                        return;  // no race condition here: PulseAll can only be reached once we hit Wait()
-
-                    Monitor.Wait(m_lock);  // wait for a changed pulse
-                }
+                Monitor.Wait(m_lock);  // wait for a changed pulse
             }
         }
 
@@ -27,7 +18,6 @@ namespace XSLibrary.ThreadSafety.Locks
         {
             lock (m_lock)
             {
-                currentWait++;
                 Monitor.PulseAll(m_lock);  // all waiting threads will resume once we release valueLock
             }
         }
