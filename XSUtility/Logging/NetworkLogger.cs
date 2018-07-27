978db31d28e187c627ad4dc5de6c34b3f7cbc648
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-27 13:54:28 +0200

    network logger is now using actor to send messages

--- XSUtility/Logging/NetworkLogger.cs
+++ XSUtility/Logging/NetworkLogger.cs
@@ -1,16 +1,48 @@
 ﻿using System.Text;
+using XSLibrary.MultithreadingPatterns.Actor;
 using XSLibrary.Network.Connections;
 
 namespace XSLibrary.Utility
 {
+    /// <summary>
+    /// Logging the same connection which is used for logging results (obviously) in an endless loop
+    /// </summary>
     public class NetworkLogger : Logger
     {
-        public IConnection Connection { get; set; }
+        class LogActor : Actor<string>
+        {
+            public IConnection Connection { get; set; }
+
+            public LogActor() : base("Network log")
+            {
+
+            }
+
+            protected override void HandleMessage(string message)
+            {
+                if (Connection != null)
+                    Connection.Send(Encoding.ASCII.GetBytes(message));
+            }
+        }
+
+        public IConnection Connection
+        {
+            get { return actor.Connection; }
+            set { actor.Connection = value; }
+        }
+
+        LogActor actor = new LogActor();
 
         protected override void LogMessage(string text)
         {
-            if (Connection != null)
-                Connection.Send(Encoding.ASCII.GetBytes(text));
+            // use actor to send messages to avoid any kind of stalling
+            actor.SendMessage(text);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            actor.Stop(false);
         }
     }
 }
