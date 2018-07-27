4538fe8b102b76eafb3fcd3a7f81e028a54dc6c1
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-07-27 09:05:39 +0200

    project file is now standalone without solution

---
+++ XSMultithreadingPatterns/UniquePair/PairingLogic.cs
@@ -0,0 +1,99 @@
+﻿namespace XSLibrary.MultithreadingPatterns.UniquePair
+{
+    class PairingLogic
+    {
+        public int ThreadCount { get; private set; }
+        public int StepCount { get; private set; }
+        public int StackCount { get; private set; }
+        public StackIDs[][] PairMatrix { get; private set; }
+
+        public PairingLogic(int threadCount)
+        {
+            InitializeStatics(threadCount);
+            InitializeMatrix();
+        }
+
+        private void InitializeStatics(int threadCount)
+        {
+            ThreadCount = threadCount;
+            StepCount = (2 * ThreadCount) - 1;
+            StackCount = 2 * ThreadCount;
+        }
+
+        private void InitializeMatrix()
+        {
+            PairMatrix = new StackIDs[StepCount][];
+
+            int[] IDs = CreateBaseArray();
+
+            for (int step = 0; step < StepCount; step++)
+            {
+                PairMatrix[step] = new StackIDs[ThreadCount];
+                for (int threadID = 0; threadID < ThreadCount; threadID++)
+                {
+                    StackIDs ids;
+                    ids.ID1 = IDs[threadID];
+                    ids.ID2 = IDs[StackCount - 1 - threadID];
+                    PairMatrix[step][threadID] = ids;
+                }
+
+                if(step + 1 < StepCount)
+                    ShiftArray(IDs);
+            }
+        }
+
+        private int[] CreateShiftedArray(int step)
+        {
+            int[] IDs = CreateBaseArray();
+
+            for (int i = 0; i < step; i++)
+            {
+                ShiftArray(IDs);
+            }
+
+            return IDs;
+        }
+
+        private int[] CreateBaseArray()
+        {
+            int[] baseIDs = new int[StackCount];
+
+            for (int i = 0; i < StackCount; i++)
+            {
+                baseIDs[i] = i;
+            }
+
+            return baseIDs;
+        }
+
+        private void ShiftArray(int[] array, int times)
+        {
+            for (int i = 0; i < times; i++)
+                ShiftArray(array);
+        }
+
+
+        private void ShiftArray(int[] array)
+        {
+            for (int i = 1; i < StackCount; i++)
+            {
+                array[i] = CircleInt(array[i] + 1);
+                if (array[i] == 0)
+                    array[i]++;
+            }
+        }
+
+        private int CircleInt(int value)
+        {
+            int cap = ThreadCount * 2;
+
+            if (value < 0)
+                return CircleInt(value + cap);
+
+            if (value >= cap)
+                return CircleInt(value - cap);
+
+            return value;
+        }
+    }
+}
---
+++ XSMultithreadingPatterns/UniquePair/UniquePairThreading.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using XSLibrary.MultithreadingPatterns.UniquePair.DistributionNodes;
+
+namespace XSLibrary.MultithreadingPatterns.UniquePair
+{
+    static class DDLExports
+    {
+        [DllImport("kernel32.dll")]
+        public static extern int GetCurrentThreadId();
+    }
+
+    struct StackIDs
+    {
+        public int ID1;
+        public int ID2;
+    }
+
+    public partial class RoundRobinTournamentDistribution<PartType, GlobalDataType> : UniquePairDistribution<PartType, GlobalDataType>
+    {
+        PairingLogic PairLogic { get; set; }
+        public int ThreadCount { get { return PairLogic.ThreadCount; } }
+        public int StepCount { get { return PairLogic.StepCount; } }
+        int StackCount { get { return PairLogic.StackCount; } }
+
+        PartType[][] Stacks { get; set; }
+        GlobalDataType GlobalData { get; set; }
+
+        public RoundRobinTournamentDistribution(DistributionPool<PartType, GlobalDataType> pool) : base(pool)
+        {
+            PairLogic = new PairingLogic(pool.NodeCount); // needs to be initialized first so all the variables used are intiialized as well
+
+            Stacks = new PartType[StackCount][];
+        }
+
+        public override void Calculate(PartType[] parts, GlobalDataType globalData)
+        {
+            CreateStacks(parts);
+            GlobalData = globalData;
+
+            for (int i = 0; i < StepCount; i++)
+            {
+                CalculateStep(i);
+            }
+        }
+
+        public override void Dispose()
+        {
+            m_distributionPool.Dispose();
+        }
+
+        private void CreateStacks(PartType[] parts)
+        {
+            int stackSize = parts.Length / StackCount;
+            int leftover = parts.Length % StackCount;
+
+            for (int i = 0; i < StackCount; i++)
+            {
+                // as there might be numbers of parts which are not divideable cleanly
+                // the leftovers get added one by one to the first few stacks
+
+                // e.g. 6 parts divided by 4 stacks would mean Stacks[0] and Stacks[1] would hold 2 values 
+                // while Stacks[2] and Stacks[3] hold only one
+                if (i < leftover)
+                {
+                    Stacks[i] = new PartType[stackSize + 1];
+                    Array.Copy(parts, i * stackSize + i, Stacks[i], 0, stackSize + 1);
+                }
+                else
+                {
+                    Stacks[i] = new PartType[stackSize];
+                    Array.Copy(parts, i * stackSize + leftover, Stacks[i], 0, stackSize);
+                }
+            }
+        }
+
+        private void CalculateStep(int step)
+        {
+            for (int i = 0; i < PairLogic.ThreadCount; i++)
+            {
+                m_distributionPool.DistributeCalculation(i, CreateCalculationPair(i, step));
+            }
+
+            m_distributionPool.Synchronize();
+        }
+
+        private CalculationPair<PartType, GlobalDataType> CreateCalculationPair(int threadID, int step)
+        {
+            //int stackID1 = CircleInt(threadID + step);
+            //int stackID2 = GetCalculationPartner(threadID, step, step >= ThreadCount);
+
+            CalculationPair<PartType, GlobalDataType> data = new CalculationPair<PartType, GlobalDataType>(
+                Stacks[PairLogic.PairMatrix[step][threadID].ID1],
+                Stacks[PairLogic.PairMatrix[step][threadID].ID2],
+                GlobalData,
+                step == 0
+                );
+
+            //data.Step = step;
+            //data.ThreadID = threadID;
+            //data.StackID = PairLogic.PairMatrix[step][threadID];
+
+            return data;
+        }
+    }
+}
