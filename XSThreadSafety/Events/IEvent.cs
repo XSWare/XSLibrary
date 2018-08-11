38f8917738fdb0f6423e98f24dc60c2e2feb09a8
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-08-11 11:43:25 +0200

    added IEvent interface

---
+++ XSThreadSafety/Events/IEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace XSLibrary.ThreadSafety.Events
+{
+    public abstract class IEvent<Sender, Args>
+    {
+        public delegate void EventHandle(Sender sender, Args arguments);
+
+        /// <summary>
+        /// Handle will be invoked if the event was triggered in the past.
+        /// <para>Unsubscribing happens automatically after the invocation and is redundant if done from the event handle.</para>
+        /// </summary>
+        public abstract event EventHandle Event;
+    }
+}
