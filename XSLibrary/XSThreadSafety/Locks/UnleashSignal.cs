d1b223d026254f4bd313b433e7d441551586c784
Adrian Thoenig <thoenig.adrian@hotmail.com>
2018-10-28 23:25:38 +0100

    Documentation explaining what the Unleash Singal is about

--- XSLibrary/XSThreadSafety/Locks/UnleashSignal.cs
+++ XSLibrary/XSThreadSafety/Locks/UnleashSignal.cs
@@ -5,6 +5,9 @@ namespace XSLibrary.ThreadSafety.Locks
 {
     public class UnleashSignal : ILock, IDisposable
     {
+        // The UnleashSignal is basically like the AutoResetEvent with the additional convenience of having multiple Threads being able to wait for the same release.
+        // With the AutoResetEvent if multiple threads are using the same AutoResetEvent and are calling the WaitOne-Function only one thread will be able to continue.
+        // With the UnleashSignal all of the threads currently in the Lock method will be able to continue once released is called.
         int currentlyWaiting = 0;
         private object m_lock = new object();
         volatile bool destroyed = false;
