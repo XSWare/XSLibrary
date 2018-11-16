d102190c07c56c70bce12a523cf744e52332ded4
CaptainOachkatzl <dave.hofer@hotmail.com>
2018-11-16 12:30:36 +0100

    roughly 50x times faster via unsafe lookup table

--- XSUtility/HexStringConverter.cs
+++ XSUtility/HexStringConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace XSLibrary.Utility
 {
@@ -15,7 +16,42 @@ namespace XSLibrary.Utility
 
         public static string ToString(byte[] hexBytes)
         {
-            return BitConverter.ToString(hexBytes).Replace("-", "");
+            return ByteArrayToHexViaLookup32UnsafeDirect(hexBytes);
+        }
+
+        // https://stackoverflow.com/questions/311165/how-do-you-convert-a-byte-array-to-a-hexadecimal-string-and-vice-versa/24343727#24343727
+
+        private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
+        private unsafe static readonly uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe, GCHandleType.Pinned).AddrOfPinnedObject();
+
+        private static uint[] CreateLookup32Unsafe()
+        {
+            var result = new uint[256];
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("X2");
+                if (BitConverter.IsLittleEndian)
+                    result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+                else
+                    result[i] = ((uint)s[1]) + ((uint)s[0] << 16);
+            }
+            return result;
+        }
+
+        public unsafe static string ByteArrayToHexViaLookup32UnsafeDirect(byte[] bytes)
+        {
+            var lookupP = _lookup32UnsafeP;
+            var result = new string((char)0, bytes.Length * 2);
+            fixed (byte* bytesP = bytes)
+            fixed (char* resultP = result)
+            {
+                uint* resultP2 = (uint*)resultP;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    resultP2[i] = lookupP[bytesP[i]];
+                }
+            }
+            return result;
         }
     }
 }
