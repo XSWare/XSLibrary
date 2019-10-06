02e6284c51c4871a8c62da876c45ec531cea0f13
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-10-06 09:17:17 +0200

    added secure acceptor

---
+++ XSNetwork/Acceptors/SecureAcceptor.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Sockets;
+using XSLibrary.Cryptography.ConnectionCryptos;
+using XSLibrary.Network.Connections;
+using XSLibrary.Utility;
+
+namespace XSLibrary.Network.Acceptors
+{
+    public delegate void ConnectionCreatedHandler(object sender, TCPPacketConnection connection);
+
+    public class SecureAcceptor
+    {
+        public event ConnectionCreatedHandler SecureConnectionEstablished;
+
+        public int CryptoHandshakeTimeout { get; set; } = 5000;
+        public CryptoType Crypto { get; set; } = CryptoType.NoCrypto;
+
+        public Logger Logger { get; set; }
+        TCPAcceptor Acceptor { get; set; }
+
+        public SecureAcceptor(TCPAcceptor acceptor)
+        {
+            Acceptor = acceptor;
+            Acceptor.ClientConnected += OnBaseAcceptorClientConnect;
+        }
+
+        public void Dispose()
+        {
+            Acceptor.Dispose();
+        }
+
+        public void Run()
+        {
+            Acceptor.Run();
+        }
+
+        private void OnBaseAcceptorClientConnect(object sender, Socket acceptedSocket)
+        {
+            TCPPacketConnection connection = new TCPPacketConnection(acceptedSocket);
+            connection.Logger = Logger;
+
+            if (!connection.InitializeCrypto(CryptoFactory.CreateCrypto(Crypto, false), CryptoHandshakeTimeout))
+                return;
+
+            SecureConnectionEstablished.Invoke(this, connection);
+        }
+    }
+}
