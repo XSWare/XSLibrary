e370023b7833dcc45d5a77d1cdcd3556935fdd6b
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-10-06 09:16:13 +0200

    added secure connector

---
+++ XSNetwork/Connectors/SecureConnector.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Sockets;
+using XSLibrary.Cryptography.ConnectionCryptos;
+using XSLibrary.Network.Connections;
+using XSLibrary.Utility;
+
+namespace XSLibrary.Network.Connectors
+{
+    public class SecureConnector : Connector<TCPPacketConnection>
+    {
+        public static string MessageInitiatingHandshake { get; set; } = "Encrypting connection...";
+        public static string MessageHandshakeFailed { get; set; } = "Handshake failed.";
+
+        public CryptoType Crypto { get; set; } = CryptoType.NoCrypto;
+        public int TimeoutCryptoHandshake { get; set; } = 5000;
+
+        protected override TCPPacketConnection InitializeConnection(Socket connectedSocket)
+        {
+            TCPPacketConnection connection = new TCPPacketConnection(connectedSocket);
+
+            Logger.Log(LogLevel.Information, MessageInitiatingHandshake);
+            if (!connection.InitializeCrypto(CryptoFactory.CreateCrypto(Crypto, true), TimeoutCryptoHandshake))
+                throw new ConnectException(MessageHandshakeFailed);
+
+            return connection;
+        }
+    }
+}
