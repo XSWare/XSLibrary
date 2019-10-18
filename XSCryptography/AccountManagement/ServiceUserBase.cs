4ff4b34e5a0c9f5c0132f1afe60c8526b2b8d5dd
CaptainOachkatzl <dave.hofer@hotmail.com>
2019-10-18 09:59:13 +0200

    improvements and cleanup
    contact/username now limited to 256 characters

--- XSCryptography/AccountManagement/ServiceUserBase.cs
+++ XSCryptography/AccountManagement/ServiceUserBase.cs
@@ -23,18 +23,25 @@ namespace XSLibrary.Cryptography.AccountManagement
         {
             string query = "INSERT INTO Accounts VALUES (@username, @passwordhash, @salt, @difficulty, @accesslevel, @contact)";
 
-            SqlCommand command = new SqlCommand(query, sqlExecutor.Connection);
+            try
+            {
+                SqlCommand command = new SqlCommand(query, sqlExecutor.Connection);
 
-            command.Parameters.AddWithValue("@username", userData.Username);
-            command.Parameters.AddWithValue("@passwordhash", userData.PasswordHash);
-            command.Parameters.AddWithValue("@salt", userData.Salt);
-            command.Parameters.AddWithValue("@difficulty", userData.Difficulty);
-            command.Parameters.AddWithValue("@accesslevel", userData.AccessLevel);
-            command.Parameters.AddWithValue("@contact", userData.Contact);
+                command.Parameters.AddWithValue("@username", userData.Username);
+                command.Parameters.AddWithValue("@passwordhash", userData.PasswordHash);
+                command.Parameters.AddWithValue("@salt", userData.Salt);
+                command.Parameters.AddWithValue("@difficulty", userData.Difficulty);
+                command.Parameters.AddWithValue("@accesslevel", userData.AccessLevel);
+                command.Parameters.AddWithValue("@contact", userData.Contact);
 
-            sqlExecutor.Execute(command.ExecuteScalar);
+                return sqlExecutor.Execute(command.ExecuteNonQuery) > 0;
+            }
+            catch (SqlException ex)
+            {
+                Logger.Log(LogLevel.Error, "Error while adding to database: {0}", ex.Message);
+            }
 
-            return true;
+            return false;
         }
 
         protected override PasswordHash CreateHashAlgorithm()
@@ -44,22 +51,18 @@ namespace XSLibrary.Cryptography.AccountManagement
 
         protected override bool EraseAccountUnsafe(string username)
         {
-            if (!IsAccountExisting(username, ""))
-                return false;
-
             using (SqlCommand command = new SqlCommand("DELETE FROM Accounts WHERE username=@username", sqlExecutor.Connection))
             {
                 command.Parameters.AddWithValue("@username", username);
-                sqlExecutor.Execute(command.ExecuteScalar);
+                return sqlExecutor.Execute(command.ExecuteNonQuery) > 0;
             }
-            return true;
         }
 
         protected override void EraseAllAccountsUnsafe()
         {
             using (SqlCommand command = new SqlCommand("DROP TABLE Accounts", sqlExecutor.Connection))
             {
-                sqlExecutor.Execute(command.ExecuteScalar);
+                sqlExecutor.Execute(command.ExecuteNonQuery);
             }
         }
 
@@ -74,9 +77,9 @@ namespace XSLibrary.Cryptography.AccountManagement
         {
             using (SqlCommand command = new SqlCommand("SELECT * FROM Accounts WHERE username=@username", sqlExecutor.Connection))
             {
-                sqlExecutor.Connection.Open();
                 try
                 {
+                    sqlExecutor.Connection.Open();
                     command.Parameters.AddWithValue("@username", username);
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
@@ -98,7 +101,11 @@ namespace XSLibrary.Cryptography.AccountManagement
                 {
                     Logger.Log(LogLevel.Error, "Error while reading account from database: " + ex.Message);
                 }
-                finally { sqlExecutor.Connection.Close(); }
+                finally 
+                { 
+                    if(sqlExecutor.Connection.State == ConnectionState.Open)
+                        sqlExecutor.Connection.Close(); 
+                }
             }
 
             return null;
@@ -106,15 +113,12 @@ namespace XSLibrary.Cryptography.AccountManagement
 
         protected override bool IsAccountExisting(string username, string contact)
         {
-            int result = 0;
             using (SqlCommand command = new SqlCommand("SELECT COUNT(1) FROM Accounts WHERE username=@username OR contact=@contact", sqlExecutor.Connection))
             {
                 command.Parameters.AddWithValue("@username", username);
                 command.Parameters.AddWithValue("@contact", contact);
-                result = (int)sqlExecutor.Execute(command.ExecuteScalar);
+                return (int)sqlExecutor.Execute(command.ExecuteScalar) != 0;
             }
-
-            return result != 0;
         }
 
         protected override bool IsCharacterAllowed(char character)
@@ -129,7 +133,7 @@ namespace XSLibrary.Cryptography.AccountManagement
 
         protected override bool IsStringAllowed(string str)
         {
-            return base.IsStringAllowed(str) && str.Length > 0;
+            return base.IsStringAllowed(str) && str.Length > 0 & str.Length <= 256;
         }
     }
 }
